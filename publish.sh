7435ed61d133c12c11821d764d72a06b7a44d7a5
CheesyPhoenix <sindre.he.moe@gmail.com>
2023-04-18 20:27:41 +0200

    back to bin release

---
+++ publish.sh
@@ -0,0 +1,10 @@
+# cargo publish
+
+cargo aur
+rm hyprland-relative-workspace-*-x86_64.tar.gz
+cp PKGBUILD hyprland-relative-workspace-bin
+cd hyprland-relative-workspace-bin
+makepkg --printsrcinfo > .SRCINFO
+git add .SRCINFO PKGBUILD
+git commit -m "`cargo pkgid | cut -d "#" -f2`"
+#git push origin master
