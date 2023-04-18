998fa98bdeac294dda731c33daa8627d42518604
CheesyPhoenix <sindre.he.moe@gmail.com>
2023-04-18 20:48:22 +0200

    fix publish script

--- publish.sh
+++ publish.sh
@@ -1,9 +1,7 @@
 cargo publish
 
-cargo aur
-rm hyprland-relative-workspace-*-x86_64.tar.gz
-cp PKGBUILD hyprland-relative-workspace-bin
 cd hyprland-relative-workspace-bin
+sed -i "s/pkgver=[0-9]\.[0-9]\.[0-9]/pkgver=`cargo pkgid | cut -d "@" -f2`/" PKGBUILD
 makepkg --printsrcinfo > .SRCINFO
 git add .SRCINFO PKGBUILD
 git commit -m "`cargo pkgid | cut -d "#" -f2`"
