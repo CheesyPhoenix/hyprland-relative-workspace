307b984c805b9b3a02966f2f8ded8599f195dae4
CheesyPhoenix <sindre.he.moe@gmail.com>
2023-04-18 21:30:26 +0200

    adding new checksum to file

--- publish.sh
+++ publish.sh
@@ -3,7 +3,7 @@ cargo publish
 cp PKGBUILD hyprland-relative-workspace-bin
 cd hyprland-relative-workspace-bin
 sed -i "s/pkgver=[0-9]\.[0-9]\.[0-9]/pkgver=`cargo pkgid | cut -d "@" -f2`/" PKGBUILD
-makepkg -g
+makepkg -g >> PKGBUILD
 rm hyprland-relative-workspace-*.tar.gz
 makepkg --printsrcinfo > .SRCINFO
 git add .SRCINFO PKGBUILD
