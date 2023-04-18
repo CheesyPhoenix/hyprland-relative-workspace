55754724e3e589e389b448255cc08fb942df3955
CheesyPhoenix <sindre.he.moe@gmail.com>
2023-04-18 21:20:43 +0200

    order of operations...

--- publish.sh
+++ publish.sh
@@ -1,10 +1,10 @@
 cargo publish
 
-makepkg -g
-rm hyprland-relative-workspace-*.tar.gz
 cp PKGBUILD hyprland-relative-workspace-bin
 cd hyprland-relative-workspace-bin
 sed -i "s/pkgver=[0-9]\.[0-9]\.[0-9]/pkgver=`cargo pkgid | cut -d "@" -f2`/" PKGBUILD
+makepkg -g
+rm hyprland-relative-workspace-*.tar.gz
 makepkg --printsrcinfo > .SRCINFO
 git add .SRCINFO PKGBUILD
 git commit -m "`cargo pkgid | cut -d "#" -f2`"
