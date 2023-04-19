40629abf32438daaa87806a99bd99f5d0a8a687a
CheesyPhoenix <sindre.he.moe@gmail.com>
2023-04-19 18:04:39 +0200

    fix publish script?

--- publish.sh
+++ publish.sh
@@ -1,10 +1,14 @@
+pkgver=`cargo pkgid | cut -d "@" -f2`
+
 cargo publish
+
+cargo aur
+
+gh release create v$pkgver hyprland-relative-workspace-$pkgver-x86_64.tar.gz --generate-notes -t $pkgver
+cp hyprland-relative-workspace-$pkgver-x86_64.tar.gz releases/
 
 cp PKGBUILD hyprland-relative-workspace-bin
 cd hyprland-relative-workspace-bin
-sed -i "s/pkgver=[0-9]\.[0-9]\.[0-9]/pkgver=`cargo pkgid | cut -d "@" -f2`/" PKGBUILD
-makepkg -g >> PKGBUILD
-rm hyprland-relative-workspace-*.tar.gz
 makepkg --printsrcinfo > .SRCINFO
 git add .SRCINFO PKGBUILD
 git commit -m "`cargo pkgid | cut -d "#" -f2`"
